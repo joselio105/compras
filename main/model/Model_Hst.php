8af207294c7c0054c25964b944556a92f6507542
Jose Helio <jose.helio@gmail.com>
2020-06-15 16:22:53 -0300

    gerenciamento da lista de compras

--- main/model/Model_Hst.php
+++ main/model/Model_Hst.php
@@ -17,7 +17,8 @@ final class Model_Hst extends Model_Class{
             $this->setJoin('lcp_und', 'lcp_emb.unidade=lcp_und.id', array('sigla'));
             $this->setJoin('lcp_emb_tp', 'lcp_emb.tipo=lcp_emb_tp.id', array('nome'=>'emb_tp_nome'));
             $this->concat(array('lcp_pdt.nome', 'lcp_emb_tp.nome', 'lcp_emb.capacidade', 'lcp_und.sigla'), 'nome', ' ');
-            $this->setSubQuery('SELECT preco FROM lcp_hst WHERE quantidade!=0 AND lcp_hst.mercadoria=tbl.mercadoria ORDER BY data DESC LIMIT 1', 'lastPreco');
+            $this->setSubQuery('SELECT preco FROM lcp_hst WHERE data IS NOT NULL AND lcp_hst.mercadoria=tbl.mercadoria ORDER BY data DESC LIMIT 1', 'lastPreco');
+            $this->setSubQuery('SELECT quantidade FROM lcp_hst WHERE data IS NOT NULL AND lcp_hst.mercadoria=tbl.mercadoria ORDER BY data DESC LIMIT 1', 'lastQtd');
         }
     }
     
